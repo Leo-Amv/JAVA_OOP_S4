20904f39e967dacce1b78081c140846b834c47f6
Leo_Amv <leonidamvrosov@yandex.ru>
2022-12-06 20:49:20 +0500

    Init commit

---
+++ src/main/java/ru/gb/oseminar4/Main.java
@@ -0,0 +1,8 @@
+package ru.gb.oseminar4;
+
+public class Main {
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/main/java/ru/gb/oseminar4/data/User.java
@@ -0,0 +1,4 @@
+package ru.gb.oseminar4.data;
+
+public class User {
+}
