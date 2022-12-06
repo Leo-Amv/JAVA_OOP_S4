03d7eadb58f674bf83a555ba344ae31c71e5ac82
Leo_Amv <leonidamvrosov@yandex.ru>
2022-12-06 20:53:55 +0500

    DateFormat added

--- src/main/java/ru/gb/oseminar4/Main.java
+++ src/main/java/ru/gb/oseminar4/Main.java
@@ -1,8 +1,15 @@
 package ru.gb.oseminar4;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+
 public class Main {
     public static void main(String[] args)
     {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd hh:mm");
+        Date now = Calendar.getInstance().getTime();
 
+        System.out.println(dateFormat.format(now));
     }
 }
